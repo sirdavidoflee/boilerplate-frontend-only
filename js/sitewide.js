4c3cdd1892abb12115c5b35d602217d4689c0b58
Dave Lee <dlee@mullen.com>
2014-03-31 09:33:58 -0400

    first commit - finally putting my boilerplate into a repo

---
+++ js/sitewide.js
@@ -0,0 +1,26 @@
+var SS = {
+    
+/* Globals Variables
+=============================================*/    
+
+
+
+/* Methods
+=============================================*/	
+    init: function(){
+		
+    },
+        
+    ultraQuality: function(){
+		
+    },
+        
+    lowQuality: function(){
+		
+    }
+};
+
+
+$(function(){
+    SS.init();
+});
